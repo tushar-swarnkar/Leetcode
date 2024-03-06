bba5adc7dbbe229fb8374bc14c13b7c1fc8f0731
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-03-06 18:51:36 +0530

    day 06, ques 06

---
+++ DAY06_linkedListCycle.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+
+public class DAY06_linkedListCycle {
+    public static void main(String[] args) {
+        
+    }
+    static boolean hasCycle(ListNode head) {
+        HashSet<ListNode> visited_nodes = new HashSet<>();
+        ListNode current_node = head;
+        while (current_node != null) {
+            if (visited_nodes.contains(current_node)) {
+                return true;
+            }
+            visited_nodes.add(current_node);
+            current_node = current_node.next;
+        }
+        return false;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
