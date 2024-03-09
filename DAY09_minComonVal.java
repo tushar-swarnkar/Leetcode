8375a38575d8a776eec07849b273dd724f756b28
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-03-09 09:46:43 +0530

    day 09, ques 09

---
+++ DAY09_minComonVal.java
@@ -0,0 +1,25 @@
+public class DAY09_minComonVal {
+    public static void main(String[] args) {
+        int[] a = {1,2,3};
+        int[] b = {2,4};
+
+        System.out.println(getCommon(a, b));
+    }
+
+    static int getCommon(int[] nums1, int[] nums2) {
+        int i = 0;
+        int j = 0;
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        return -1;
+    }
+}
