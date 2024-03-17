52338d2f55ef2b19894d73e562fd57a24b5f8db4
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-03-17 14:30:35 +0530

    day 17, qus 17

---
+++ DAY17_insertInterval.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DAY17_insertInterval {
+    public static void main(String[] args) {
+        int[][] intervals = {{1,3}, {6,9}};
+        int[] newInterval = {2,5};
+
+        int[][] ans = insert(intervals, newInterval);
+
+        System.out.println(Arrays.toString(ans));
+    }
+    static int[][] insert(int[][] intervals, int[] newInterval) {
+		List<int[]> result = new LinkedList<>();
+	    int i = 0;
+	    // add all the intervals ending before newInterval starts
+	    while (i < intervals.length && intervals[i][1] < newInterval[0]){
+	        result.add(intervals[i]);
+	        i++;
+	    }
+	    
+	    // merge all overlapping intervals to one considering newInterval
+	    while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+	    	// we could mutate newInterval here also
+	        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+	        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+	        i++;
+	    }
+	    
+	    // add the union of intervals we got
+	    result.add(newInterval); 
+	    
+	    // add all the rest
+	    while (i < intervals.length){
+	    	result.add(intervals[i]); 
+	    	i++;
+	    }
+	    
+	    return result.toArray(new int[result.size()][]);
+    }
+}
