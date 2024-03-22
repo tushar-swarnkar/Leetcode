f1f031df92de89ab2a5ac828f8cb8e5906fb486e
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-03-22 10:43:45 +0530

    day 22, ques 22

---
+++ DAY22_palindromicLinkedList.java
@@ -0,0 +1,46 @@
+public class DAY22_palindromicLinkedList {
+    public static void main(String[] args) {
+        
+    }
+    static boolean isPalindrome(ListNode head) {
+        if(head == null) {
+            return true;
+        }
+        ListNode p1 = head;
+        ListNode p2 = head;
+        ListNode p3 = p1.next;
+        ListNode pre = p1;
+        //find mid pointer, and reverse head half part
+        while(p2.next != null && p2.next.next != null) {
+            p2 = p2.next.next;
+            pre = p1;
+            p1 = p3;
+            p3 = p3.next;
+            p1.next = pre;
+        }
+
+        //odd number of elements, need left move p1 one step
+        if(p2.next == null) {
+            p1 = p1.next;
+        }
+        else {   //even number of elements, do nothing
+            
+        }
+        //compare from mid to head/tail
+        while(p3 != null) {
+            if(p1.val != p3.val) {
+                return false;
+            }
+            p1 = p1.next;
+            p3 = p3.next;
+        }
+        return true;
+    }
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
