55abcdc8b27bee568bb481c9be94e13674f558ec
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-03-30 12:19:16 +0530

    day 30, ques 30

---
+++ DAY30_subArrWithMaxK.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+
+public class DAY30_subArrWithMaxK {
+
+    // LEETCODE 992
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,1,2,3};
+        int k = 0;
+
+        System.out.println(subarraysWithKDistinct (arr, k));
+    }
+    static int subarraysWithKDistinct(int[] nums, int k) {
+        int subWithMaxK = subarrayWithAtMostK(nums, k);
+        int reducedSubWithMaxK = subarrayWithAtMostK(nums, k - 1);
+        return subWithMaxK - reducedSubWithMaxK;
+    }
+    
+    static int subarrayWithAtMostK(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int left = 0, right = 0, ans = 0;
+        
+        while (right < nums.length) {
+            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+            
+            while (map.size() > k) {
+                map.put(nums[left], map.get(nums[left]) - 1);
+                if (map.get(nums[left]) == 0) {
+                    map.remove(nums[left]);
+                }
+                left++;
+            }
+            
+            ans += right - left + 1; // Size of subarray
+            right++;
+        }
+        
+        return ans;
+    }
+}
