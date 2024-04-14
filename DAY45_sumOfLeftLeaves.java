ced2d97d05709ec626f3c0e9c5fb8b38eb8abbc3
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-04-14 14:37:49 +0530

    day 45, ques 45

---
+++ DAY45_sumOfLeftLeaves.java
@@ -0,0 +1,39 @@
+public class DAY45_sumOfLeftLeaves {
+
+    // LEETCODE : 404
+
+    public static void main(String[] args) {
+
+    }
+
+    static int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            return root.left.val + sumOfLeftLeaves(root.right);
+        }
+
+        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
