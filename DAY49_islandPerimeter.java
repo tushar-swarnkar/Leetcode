b09e1d150bf668e40f82c698fb03f78fdbb24061
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-04-18 22:08:05 +0530

    day 49, ques 49

---
+++ DAY49_islandPerimeter.java
@@ -0,0 +1,40 @@
+public class DAY49_islandPerimeter {
+
+    // LEETCODE : 463
+
+    public static void main(String[] args) {
+        int[][] grid = {{0,1,0,0}, {1,1,1,0}, {0,1,0,0}, {1,1,0,0}};
+
+        System.out.println(islandPerimeter(grid));
+    }
+    static int islandPerimeter(int[][] grid) {
+        if (grid == null) return 0;
+        for (int i = 0 ; i < grid.length ; i++){
+            for (int j = 0 ; j < grid[0].length ; j++){
+                if (grid[i][j] == 1) {
+                    return getPerimeter(grid,i,j);
+                }
+            }
+        }
+        return 0;
+    }
+    
+    static int getPerimeter(int[][] grid, int i, int j){
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {return 1;}
+        if (grid[i][j] == 0) {
+            return 1;
+        }
+        if (grid[i][j] == -1) return 0;
+        
+        int count = 0;
+        grid[i][j] = -1;
+        
+        count += getPerimeter(grid, i-1, j);
+        count += getPerimeter(grid, i, j-1);
+        count += getPerimeter(grid, i, j+1);
+        count += getPerimeter(grid, i+1, j);
+        
+        return count;
+        
+    }
+}
