8f305d2ff48106cfcbd1e4774ec7f9f6938cb1ef
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-05-13 10:04:35 +0530

    day 75, ques 75

---
+++ DAY75_scoreAfterFlippingMatrix.java
@@ -0,0 +1,27 @@
+public class DAY75_scoreAfterFlippingMatrix {
+
+    // LEETCODE : 861
+    
+    public static void main(String[] args) {
+        
+    }
+    static int matrixScore(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        int res = (1 << (m - 1)) * n;  
+
+        for (int j = 1; j < m; ++j) {
+            int val = 1 << (m - 1 - j);
+            int set = 0;
+
+            for (int i = 0; i < n; ++i) {
+                if (grid[i][j] == grid[i][0]) {
+                    set++;
+                }
+            }
+
+            res += Math.max(set, n - set) * val;
+        }
+
+        return res;
+    }
+}
