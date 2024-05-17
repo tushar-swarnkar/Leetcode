599dae389a2ecece521215ae344ef7dccfdef481
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-05-17 21:46:21 +0530

    day 79, ques 79

---
+++ DAY79_delLeavesWithGivenValue.java
@@ -0,0 +1,37 @@
+public class DAY79_delLeavesWithGivenValue {
+
+    // LEETCODE : 1325
+
+    public static void main(String[] args) {
+
+    }
+
+    static TreeNode removeLeafNodes(TreeNode root, int target) {
+        if (root == null)
+            return null;
+        root.left = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
+        if (root.left == null && root.right == null && root.val == target)
+            return null;
+        return root;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
