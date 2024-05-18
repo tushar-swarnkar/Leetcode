45b0ad97640661ee0d9588cae830ec1dbb688374
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-05-18 21:13:47 +0530

    day 80, ques 80

---
+++ DAY80_distributeCoinsInBinaryTere.java
@@ -0,0 +1,45 @@
+import javax.swing.tree.TreeNode;
+
+public class DAY80_distributeCoinsInBinaryTere {
+
+    // LEETCODE : 979
+
+    public static void main(String[] args) {
+
+    }
+
+    static int moves = 0;
+
+    private int dfs(TreeNode node) {
+        if (node == null)
+            return 0;
+        int left_excess = dfs(node.left);
+        int right_excess = dfs(node.right);
+        moves += Math.abs(left_excess) + Math.abs(right_excess);
+        return node.val + left_excess + right_excess - 1;
+    }
+
+    public int distributeCoins(TreeNode root) {
+        dfs(root);
+        return moves;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
