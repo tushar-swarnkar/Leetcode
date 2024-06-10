1dbb18cf3faf3821f89bdcd0909ee4d0fa1d2027
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-06-10 18:16:42 +0530

    day 103, ques 103

---
+++ DAY103_heightChecker.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class DAY103_heightChecker {
+    public int heightChecker(int[] heights) {
+        int[] expected = heights.clone();
+        Arrays.sort (expected);
+        int count = 0;
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != expected[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
