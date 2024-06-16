a809c998b54f1525c8c5eac9f41cb3d32e721eef
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-06-16 15:25:00 +0530

    day 109, ques 109

---
+++ DAY109_patchingArray.java
@@ -0,0 +1,20 @@
+public class DAY109_patchingArray {
+    // LEETCODE : 330
+    public int minPatches(int[] nums, int n) {
+        long miss = 1;
+        int result = 0;
+        int i = 0;
+
+        while (miss <= n) {
+            if (i < nums.length && nums[i] <= miss) {
+                miss += nums[i];
+                i++;
+            } else {
+                miss += miss;
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
