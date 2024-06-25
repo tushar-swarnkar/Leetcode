91fd7a3782ca5d5e46f7b8d215986656406ea6aa
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-06-25 12:10:33 +0530

    day 118, ques 118

---
+++ DAY118_BSGtoGST.java
@@ -0,0 +1,33 @@
+
+public class DAY118_BSGtoGST {
+    private int sum = 0;
+
+    public TreeNode bstToGst(TreeNode root) {
+        if (root != null) {
+            bstToGst(root.right); // Traverse the right subtree
+            sum += root.val; // Update the sum
+            root.val = sum; // Update the current node's value
+            bstToGst(root.left); // Traverse the left subtree
+        }
+        return root;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
