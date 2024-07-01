93deb39273cff5b7c81e2fc4dce31ce1ef7db79c
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-07-01 21:43:59 +0530

    day 124, ques 124

---
+++ DAY124_threeConsecutiveOdds.java
@@ -0,0 +1,18 @@
+public class DAY124_threeConsecutiveOdds {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                count++;
+                if (count == 3) {
+                    return true;
+                }
+            } else {
+                count = 0;
+            }
+        }
+
+        return false;
+    }
+}
