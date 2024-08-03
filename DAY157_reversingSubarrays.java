6d3f21ef5a655a2d43977b7df095a24c4d60d758
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-08-03 10:11:39 +0530

    day 157, ques 157

---
+++ DAY157_reversingSubarrays.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class DAY157_reversingSubarrays {
+    public boolean canBeEqual(int[] a, int[] b) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < a.length; i++) {
+            map.put(a[i], map.getOrDefault(a[i], 0) + 1);
+            map.put(b[i], map.getOrDefault(b[i], 0) - 1);
+        }
+        for (int i : map.keySet()) {
+            if (map.get(i) != 0)
+                return false;
+        }
+        return true;
+    }
+}
