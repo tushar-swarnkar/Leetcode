6b2af55e7c88794ca2268aeb62022c4b531d0aea
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-08-19 20:37:08 +0530

    day 173, ques 173

---
+++ DAY173_2WordKeyboard.java
@@ -0,0 +1,20 @@
+public class DAY173_2WordKeyboard {
+
+    public int minSteps(int n) {
+        if (n == 1)
+            return 0;
+
+        int steps = 0;
+        int factor = 2;
+
+        while (n > 1) {
+            while (n % factor == 0) {
+                steps += factor;
+                n /= factor;
+            }
+            factor++;
+        }
+
+        return steps;
+    }
+}
