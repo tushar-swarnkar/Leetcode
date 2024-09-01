c17d86541ab0fc0be53ae03a98d7b2696ce4c3b7
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-09-01 21:38:03 +0530

    day 86, ques 86

---
+++ DAY186_1dTo2dArrayConversion.java
@@ -0,0 +1,18 @@
+public class DAY186_1dTo2dArrayConversion {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+
+        if (m * n != original.length) {
+            return new int[0][0];
+        }
+
+        int[][] arr2D = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr2D[i][j] = original[i * n + j];
+            }
+        }
+
+        return arr2D;
+    }
+}
