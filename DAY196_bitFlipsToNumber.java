997186c0c8b8882875414ad3773d012749fdb7a7
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-09-11 21:32:12 +0530

    day 196, ques 196

---
+++ DAY196_bitFlipsToNumber.java
@@ -0,0 +1,12 @@
+public class DAY196_bitFlipsToNumber {
+    public int minBitFlips(int start, int goal) {
+        int ans = 0;
+        int xor = start ^ goal;
+
+        while (xor != 0) {
+            ans += xor & 1;
+            xor >>= 1;
+        }
+        return ans;
+    }
+}
