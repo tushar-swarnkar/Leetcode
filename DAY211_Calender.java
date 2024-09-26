629c02364a933437be9e2a0f006ee4db84732649
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-09-26 21:39:00 +0530

    day 211, ques 211

---
+++ DAY211_Calender.java
@@ -0,0 +1,20 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public class DAY211_Calender {
+    class MyCalendar {
+        TreeMap<Integer, Integer> calendar = new TreeMap<>();
+
+        public MyCalendar() {
+            calendar.put(Integer.MAX_VALUE, Integer.MAX_VALUE);
+        }
+
+        public boolean book(int start, int end) {
+            Map.Entry<Integer, Integer> pair = calendar.higherEntry(start);
+            boolean res = end <= pair.getValue();
+            if (res)
+                calendar.put(end, start);
+            return res;
+        }
+    }
+}
