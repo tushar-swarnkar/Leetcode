59dde6033e5e128b2cbc9a4a03945fd76eedec27
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-10-28 13:46:54 +0530

    day 243, ques 243

---
+++ DAY243_longestSquareStreak.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class DAY243_longestSquareStreak {
+    public int longestSquareStreak(int[] A) {
+        HashMap<Integer, Integer> dp = new HashMap<>();
+        int res = 0;
+        Arrays.sort(A);
+        for (var i : A) {
+            int root = (int) Math.sqrt(i);
+            if (root * root == i)
+                dp.put(i, dp.getOrDefault(root, 0) + 1);
+            else
+                dp.put(i, 1);
+            res = Math.max(res, dp.get(i));
+        }
+        return res < 2 ? -1 : res;
+    }
+}
