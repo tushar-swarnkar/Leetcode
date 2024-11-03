bd41bf1f8730f75077c0c944f4e8c4b59fd9729f
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-11-03 22:45:56 +0530

    day 249, ques 249

---
+++ DAY249_rotateString.java
@@ -0,0 +1,31 @@
+public class DAY249_rotateString {
+
+    public boolean rotateString(String A, String B) {
+        if (A == null || B == null) {
+            // throw exception on A and B both being null?
+            return false;
+        }
+        if (A.length() != B.length()) {
+            return false;
+        }
+        if (A.length() == 0) {
+            return true;
+        }
+        for (int i = 0; i < A.length(); i++) {
+            if (rotateString(A, B, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean rotateString(String A, String B, int rotation) {
+        for (int i = 0; i < A.length(); i++) {
+            if (A.charAt(i) != B.charAt((i + rotation) % B.length())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
