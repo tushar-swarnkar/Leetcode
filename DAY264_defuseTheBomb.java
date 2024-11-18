55c8e8fe327d8a2b133b628878f65c05aee85094
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-11-18 23:45:36 +0530

    day 264, ques 264

---
+++ DAY264_defuseTheBomb.java
@@ -0,0 +1,22 @@
+public class DAY264_defuseTheBomb {
+
+    public int[] decrypt(int[] code, int k) {
+        int N = code.length;
+        int[] res = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            if (k > 0) {
+                for (int j = i + 1; j < i + 1 + k; j++) {
+                    res[i] += code[j % N];
+                }
+            } else if (k < 0) {
+                for (int j = i - 1; j > i - 1 - Math.abs(k); j--) {
+                    res[i] += code[((j % N) + N) % N];
+                }
+            }
+        }
+
+        return res;
+    }
+
+}
