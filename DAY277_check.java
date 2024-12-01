32fc7d08115b4cb7a65bc86c841f29ed22339f1a
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-12-02 01:42:23 +0530

    day 277, ques 277

---
+++ DAY277_check.java
@@ -0,0 +1,13 @@
+public class DAY277_check {
+
+    public boolean checkIfExist(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (i != j && arr[i] == 2 * arr[j])
+                    return true;
+
+        return false;
+    }
+
+}
