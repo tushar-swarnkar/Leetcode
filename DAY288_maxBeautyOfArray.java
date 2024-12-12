a3d3005631e5e04c206591feaa546f7ffd07951e
Tushar Swarnkar <swarnkar.tushar03@gmail.com>
2024-12-12 23:31:40 +0530

    day 288, ques 288

---
+++ DAY288_maxBeautyOfArray.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class DAY288_maxBeautyOfArray {
+    public int maximumBeauty(int[] A, int k) {
+        Arrays.sort(A);
+        int i = 0, j, n = A.length;
+        for (j = 0; j < n; ++j)
+            if (A[j] - A[i] > k * 2)
+                i++;
+        return j - i;
+    }
+}
